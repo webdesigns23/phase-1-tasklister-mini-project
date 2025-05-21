54dab87d0d77cb449177d45700436c518c5360f3
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-21 08:48:29 -0600

    added js to html form to show users todos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+//Select Form:
+const form = document.querySelector("#create-task-form");
+
+//Select Task List to display task:
+const taskList = document.querySelector('#tasks');
+
+//Add event listener to submit form with preventDefault
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  
+  //to get user input from the box with name new-task-description
+  const inputNewTask = document.querySelector("#new-task-description").value
+   
+  //pass input task
+  buildToDo(inputNewTask,event)
+
 });
+
+//add input task to list and display
+function buildToDo(inputNewTask) {
+
+  const list = document.createElement('li')
+  list.textContent = inputNewTask
+ 
+  //add list to task list
+  taskList.append(list)
+}
+
+});
+
+
+
